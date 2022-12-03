48de178e53bd1de732714447be18ae4275dcafdd
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-12-03 02:59:45 +0000

    Chapter04

---
+++ Chapter04.java
@@ -0,0 +1,13 @@
+public class Chapter04 {
+  public static void main(String[] args) {
+    String str1;
+    str1 = "Miyakawa Dan";
+    System.out.println(str1);
+
+    str1 = "aaa";
+    System.out.println(str1);
+
+    final int NUM = 77;
+    System.out.println(NUM);
+  }
+}
