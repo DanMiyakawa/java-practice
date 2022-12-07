e5d5b87e6d22dce571b9eed50db439ec0fb7cfa9
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-12-07 05:29:13 +0000

    Chapter10

---
+++ Exception02.java
@@ -0,0 +1,13 @@
+public class Exception02{
+  public static void main(String[] args){
+    try{
+      System.out.println("100 ÷ 0 は？");
+      int result = 100 / 0;
+      System.out.println("計算結果" + result);
+    }catch (ArithmeticException e){
+      System.out.println("例外が発生");
+    }finally{
+      System.out.println("プログラム修了");
+    }
+  }
+}
