ae324b2f0160655638dd6ef6cfc9722ec6a4783d
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-12-08 01:08:52 +0000

    Chapter12

---
+++ Chapter10.java
@@ -0,0 +1,34 @@
+package practice;
+
+import java.sql.SQLException;
+
+public class Chapter10 {
+    public static void main(String[] args) {
+
+        try {
+            int array[] = { 1, 3, 5 };
+            Chapter10.validIndex(array, 2);
+            Chapter10.validIndex(array, 3);
+
+            System.out.println("validIndexの呼び出し終了");
+        } catch (IllegalArgumentException e) {
+            System.out.println("IllegalArgumentExceptionが発生しました");
+            e.printStackTrace();
+        }
+
+        // try-catch文を記述
+        // ・throwSQLExceptionメソッドを呼び出し
+        // ・SQLExceptionをキャッチして、メッセージとスタックトレースを出力
+        // ・"throwSQLExceptionの呼び出し終了"のメッセージを出力
+
+        System.out.println("mainメソッド終了");
+    }
+
+    // validIndexメソッドを作成
+    // ・indexがarrayのサイズの範囲内なら、インデックスの要素を出力
+    // ・サイズの範囲外なら、IllegalArgumentExceptionをスロー
+
+    public static void throwSQLException() throws SQLException {
+        throw new SQLException("SQLエラーです");
+    }
+}
