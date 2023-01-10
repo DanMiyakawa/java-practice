60f51eb0eb5793b76bd709e4d0626a634b605cbc
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2023-01-10 00:05:30 +0000

    Chapter12

---
+++ NoCapsuleMan.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class NoCapsuleMan {
+    public String name;
+    public int age;
+
+    public NoCapsuleMan(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
